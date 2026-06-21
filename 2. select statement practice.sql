e169c34e4cdc0ca5ea215d80b67ea1623783f6c8
ishwarilonkar2005-ui <ishwarilonkar2005@gmail.com>
2026-06-21 22:28:17 +0530

    Update 2. select statement practice.sql

--- 2. select statement practice.sql
+++ 2. select statement practice.sql
@@ -1,9 +1,10 @@
+-- SELECT clause
 SELECT * 
 FROM employee_demographics;
 
 SELECT first_name, last_name, birth_date,age,(age+10) *10
 FROM employee_demographics;
-#pemdas is used in mathematics of my sql
+#pemdas is used in mathsof my sql
 
 select DISTINCT first_name
 from employee_demographics;
@@ -11,4 +12,41 @@ from employee_demographics;
 SELECT DISTINCT first_name, gender
 FROM employee_demographics;
 
+-- WHERE clause
+SELECT *
+FROM employee_salary
+where salary >= 50000;
 
+SELECT *
+FROM employee_demographics
+where gender != 'female';
+
+select *
+from employee_demographics
+where birth_date > '1985-01-01';
+
+-- and or not -- logical operators
+select *
+from employee_demographics
+where birth_date > '1985-01-01'
+and gender = 'male';
+
+select *
+from employee_demographics
+where birth_date > '1985-01-01'
+or gender = 'male';
+
+select *
+from employee_demographics
+where birth_date > '1985-01-01'
+or not  gender = 'male';
+
+select *
+from employee_demographics
+where (first_name='leslie' and age= 44) or age>55;
+
+-- like statement
+-- % and_
+select *
+from employee_demographics
+where birth_date like '1989%';
